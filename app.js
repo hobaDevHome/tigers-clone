438e361b31dfbe0a6ca3bbcafb93f13d5dd26306
hoba-android <heba96@yahoo.com>
2021-11-30 08:07:33 +0200

    search

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-document.querySelector('#menu').addEventListener('click', () => {
-  console.log('clicked');
-  document.querySelector('nav').classList.toggle('showmenu');
+// document.querySelector('#menu').addEventListener('click', () => {
+//   console.log('clicked');
+//   document.querySelector('nav').classList.toggle('showmenu');
+// });
+
+// document.querySelector('#men').addEventListener('click', () => {
+//   document.querySelector('nav').classList.toggle('showmenu');
+// });
+
+document.querySelector('#search-btn-div').addEventListener('click', () => {
+  document.querySelector('.search-input-div ').classList.toggle('search-show');
+});
+document.querySelector('#close').addEventListener('click', () => {
+  document.querySelector('.search-input-div ').classList.toggle('search-show');
 });
 
-document.querySelector('#men').addEventListener('click', () => {
-  document.querySelector('nav').classList.toggle('showmenu');
+document.querySelector('#sticky-arrow').addEventListener('click', () => {
+  console.log('sticky-arrow clicked');
+  document.querySelector('.sticky-arrow').classList.toggle('arrow-donw');
+  document.querySelector('.sticky-body').classList.toggle('body-donw');
 });
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
@@ -21,70 +34,70 @@ sr.reveal(`.animat`, {
 
 /*==================== SHOW SCROLL TOP ====================*/
 function scrollTop() {
-  const scrollTop = document.getElementById('scroll-top');
+  const scrollTop = document.getElementById('search-bar');
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 560) scrollTop.classList.add('show-scroll');
-  else scrollTop.classList.remove('show-scroll');
+  if (this.scrollY >= 560) scrollTop.classList.add('move-searvh-bar');
+  else scrollTop.classList.remove('move-searvh-bar');
 }
 window.addEventListener('scroll', scrollTop);
 /*==================== products carousel ====================*/
-productScroll();
+// productScroll();
 
-function productScroll() {
-  let slider = document.getElementById("slider");
-  let next = document.getElementsByClassName("pro-next");
-  let prev = document.getElementsByClassName("pro-prev");
-  let slide = document.getElementById("slide");
-  let item = document.getElementById("slide");
+// function productScroll() {
+//   let slider = document.getElementById("slider");
+//   let next = document.getElementsByClassName("pro-next");
+//   let prev = document.getElementsByClassName("pro-prev");
+//   let slide = document.getElementById("slide");
+//   let item = document.getElementById("slide");
 
-  for (let i = 0; i < next.length; i++) {
-    //refer elements by class name
+//   for (let i = 0; i < next.length; i++) {
+//     //refer elements by class name
 
-    let position = 0; //slider postion
+//     let position = 0; //slider postion
 
-    prev[i].addEventListener("click", function() {
-      //click previos button
-      if (position > 0) {
-        //avoid slide left beyond the first item
-        position -= 1;
-        translateX(position); //translate items
-      }
-    });
+//     prev[i].addEventListener("click", function() {
+//       //click previos button
+//       if (position > 0) {
+//         //avoid slide left beyond the first item
+//         position -= 1;
+//         translateX(position); //translate items
+//       }
+//     });
 
-    next[i].addEventListener("click", function() {
-      if (position >= 0 && position < hiddenItems()) {
-        //avoid slide right beyond the last item
-        position += 1;
-        translateX(position); //translate items
-      }
-    });
-  }
+//     next[i].addEventListener("click", function() {
+//       if (position >= 0 && position < hiddenItems()) {
+//         //avoid slide right beyond the last item
+//         position += 1;
+//         translateX(position); //translate items
+//       }
+//     });
+//   }
 
-  function hiddenItems() {
-    //get hidden items
-    let items = getCount(item, false);
-    let visibleItems = slider.offsetWidth / 210;
-    return items - Math.ceil(visibleItems);
-  }
-}
+//   function hiddenItems() {
+//     //get hidden items
+//     let items = getCount(item, false);
+//     let visibleItems = slider.offsetWidth / 210;
+//     return items - Math.ceil(visibleItems);
+//   }
+// }
 
-function translateX(position) {
-  //translate items
-  slide.style.left = position * -210 + "px";
-}
+// function translateX(position) {
+//   //translate items
+//   slide.style.left = position * -210 + "px";
+// }
 
-function getCount(parent, getChildrensChildren) {
-  //count no of items
-  let relevantChildren = 0;
-  let children = parent.childNodes.length;
-  for (let i = 0; i < children; i++) {
-    if (parent.childNodes[i].nodeType != 3) {
-      if (getChildrensChildren)
-        relevantChildren += getCount(parent.childNodes[i], true);
-      relevantChildren++;
-    }
-  }
-  return relevantChildren;
-}
+// function getCount(parent, getChildrensChildren) {
+//   //count no of items
+//   let relevantChildren = 0;
+//   let children = parent.childNodes.length;
+//   for (let i = 0; i < children; i++) {
+//     if (parent.childNodes[i].nodeType != 3) {
+//       if (getChildrensChildren)
+//         relevantChildren += getCount(parent.childNodes[i], true);
+//       relevantChildren++;
+//     }
+//   }
+//   return relevantChildren;
+// }
 
 // ===========================================
